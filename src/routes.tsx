4279624e75ee6c3cc0c6a15f75260e9edebd2fa7
marciovz <marciovz@yahoo.com.br>
2024-03-30 15:22:06 -0300

    Cadastro de restaurante

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { Dashboard } from "./pages/app/dashboard";
-import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +19,7 @@ export const router = createBrowserRouter([
     element: <AuthLayout />,
     children: [
       { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
     ]
   }
 ]) 
