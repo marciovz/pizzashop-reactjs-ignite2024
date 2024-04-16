86e04a3db52d465d4063368d2220e83213e8a532
marciovz <marciovz@yahoo.com.br>
2024-04-16 18:25:05 -0300

    Atualizando perfil

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -19,11 +19,13 @@ export function AccountMenuProps() {
   const { data: profile, isLoading: isLoadingProfile  } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
+    staleTime: Infinity,
   })
   
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
+    staleTime: Infinity,
   })
 
   return (
