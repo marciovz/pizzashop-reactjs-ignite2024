04dd7543b8fae586ee1e521e28e6c541c381f99c
marciovz <marciovz@yahoo.com.br>
2024-04-17 14:52:09 -0300

    Paginação dos pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { useQuery } from "@tanstack/react-query";
+import { z } from "zod";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./oder-table-filters";
 import { Pagination } from "@/components/pagination";
@@ -7,47 +9,66 @@ import { getOrders } from "@/api/get-orders";
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export function Orders() {
+  const [ searchParams, setSearchParams ] = useSearchParams();
+
+  const pageIndex = z.coerce.number()
+    .transform(page => page - 1)
+    .parse(searchParams.get('page') ?? '1');
 
   const { data: result } = useQuery({
-    queryKey: ['orders'],
-    queryFn: getOrders,
+    queryKey: ['orders', pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   });
 
-  return(
-<>
-  <Helmet title="Pedidos" />
-  <div className="flex flex-col gap-4">
-    <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-  
-    <div className="space-y-2.5">
-      <OrderTableFilters />
+  function handlePaginate(pageIndex: number ) {
+    setSearchParams(state => {
+      state.set('page', (pageIndex + 1).toString());
+      return state;
+    })
+  }
 
-      <div className="border rounded-md">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[64px]"></TableHead>
-              <TableHead className="w-[140px]">Identificador</TableHead>
-              <TableHead className="w-[180px]">Realizado há</TableHead>
-              <TableHead className="w-[140px]">Status</TableHead>
-              <TableHead>Cliente</TableHead>
-              <TableHead className="w-[140px]">Total do pedido</TableHead>
-              <TableHead className="w-[164px]"></TableHead>
-              <TableHead className="w-[132px]"></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {result && result.orders.map(order => {
-              return <OrderTableRow key={order.orderId} order={order} />
-            })}
-          </TableBody>
-        </Table>
-      </div>
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
       
-      <Pagination pageIndex={0} totalCount={105} perPage={10} />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-    </div>
-  </div>
-</>
+          <div className="border rounded-md">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {result && result.orders.map(order => {
+                  return <OrderTableRow key={order.orderId} order={order} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          
+          {result &&(
+            <Pagination 
+              onPageChange={handlePaginate}
+              pageIndex={result.meta.pageIndex} 
+              totalCount={result.meta.totalCount} 
+              perPage={result.meta.perPage} 
+            />
+          )}
+
+        </div>
+      </div>
+    </>
   )
 };
