93c26705abe4f56a5e100419a0cb7e112d76d88d
marciovz <marciovz@yahoo.com.br>
2024-04-20 10:28:52 -0300

    Métricas no dashboard

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -1,21 +1,47 @@
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { useQuery } from '@tanstack/react-query';
 import { Utensils } from 'lucide-react'
 
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { getDayOrdersAmount } from '@/api/get-day-orders-amount';
+
 export function DayOrdersAmountCard() {
+  const { data: dayOrdersAmount } = useQuery({
+    queryFn: getDayOrdersAmount,
+    queryKey: ['metrics', 'day-orders-amount']
+  });
+
   return(
     <Card>
-    <CardHeader className="flex-row items-center space-y-0 justify-between pb-2">
-      <CardTitle className="text-base font-semibold">Pedido (dia)</CardTitle>
-      <Utensils className="h-4 w-4 text-muted-foreground" />
-    </CardHeader>
-    <CardDescription className="space-y-1">
-      <span className="text-2xl font-bold tracking-tight">
-        12
-      </span>
-      <p className="text-xs text-muted-foreground">
-        <span className="text-rose-500 dark:text-rose-400">-4%</span> em relação ao mês passado
-      </p>
-    </CardDescription>
+      <CardHeader className="flex-row items-center space-y-0 justify-between pb-2">
+        <CardTitle className="text-base font-semibold">Pedido (dia)</CardTitle>
+        <Utensils className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent className="space-y-1">
+        {dayOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {dayOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {dayOrdersAmount.diffFromYesterday >= 0 ? (
+                <>
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    +{dayOrdersAmount.diffFromYesterday}%
+                  </span>{' '}
+                  em relação a ontem
+                </>
+              ):(
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {dayOrdersAmount.diffFromYesterday}%
+                  </span>{' '}
+                  em relação a ontem
+                </>
+              )}
+            </p>
+          </>
+        )}
+      </CardContent>
   </Card>
   )
 };
--- src/pages/app/dashboard/month-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-orders-amount-card.tsx
@@ -1,21 +1,47 @@
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Utensils } from 'lucide-react'
+import { useQuery } from '@tanstack/react-query';
+import { Utensils } from 'lucide-react';
+
+import { getMonthOrdersAmount } from '@/api/get-month-orders-amount';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export function MonthOrdersAmountCard() {
+  const { data: monthOrdersAmount } = useQuery({
+    queryFn: getMonthOrdersAmount,
+    queryKey: ['metrics', 'month-orders-amount']
+  });
+  
   return(
     <Card>
-    <CardHeader className="flex-row items-center space-y-0 justify-between pb-2">
-      <CardTitle className="text-base font-semibold">Pedido (mês)</CardTitle>
-      <Utensils className="h-4 w-4 text-muted-foreground" />
-    </CardHeader>
-    <CardDescription className="space-y-1">
-      <span className="text-2xl font-bold tracking-tight">
-        246
-      </span>
-      <p className="text-xs text-muted-foreground">
-        <span className="text-emerald-500 dark:text-emerald-400">+6%</span> em relação a ontem
-      </p>
-    </CardDescription>
-  </Card>
+      <CardHeader className="flex-row items-center space-y-0 justify-between pb-2">
+        <CardTitle className="text-base font-semibold">Pedido (mês)</CardTitle>
+        <Utensils className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent className="space-y-1">
+        {monthOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthOrdersAmount.diffFromLastMonth >= 0 ? (
+                <>
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    +{monthOrdersAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  em relação ao mês passado
+                </>
+              ):(
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {monthOrdersAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  em relação ao mês passado
+                </>
+              )}
+            </p>
+          </>
+        )}
+      </CardContent>
+    </Card>
   )
 };
