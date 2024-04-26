9256f61aaaca5c6ee62091b8765e3d3d5f890e6d
marciovz <marciovz@yahoo.com.br>
2024-04-26 11:11:46 -0300

    Teste unitário: Order Status

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -15,11 +15,11 @@ const orderStatusMap: Record<OrderStatus, string> = {
 export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
-      {status === 'pending' && (<span className="h-2 w-2 rounded-full bg-slate-400" />)}
-      {status === 'canceled' && (<span className="h-2 w-2 rounded-full bg-rose-500" />)}
-      {status === 'delivered' && (<span className="h-2 w-2 rounded-full bg-emerald-500" />)}
-      {status === 'delivering' && (<span className="h-2 w-2 rounded-full bg-amber-400" />)}
-      {status === 'processing' && (<span className="h-2 w-2 rounded-full bg-amber-400" />)}
+      {status === 'pending' && (<span data-testid="badge" className="h-2 w-2 rounded-full bg-slate-400" />)}
+      {status === 'canceled' && (<span data-testid="badge" className="h-2 w-2 rounded-full bg-rose-500" />)}
+      {status === 'delivered' && (<span data-testid="badge" className="h-2 w-2 rounded-full bg-emerald-500" />)}
+      {status === 'delivering' && (<span data-testid="badge" className="h-2 w-2 rounded-full bg-amber-500" />)}
+      {status === 'processing' && (<span data-testid="badge" className="h-2 w-2 rounded-full bg-amber-500" />)}
       
       <span className="font-medium text-muted-foreground">{orderStatusMap[status]}</span>
     </div>
