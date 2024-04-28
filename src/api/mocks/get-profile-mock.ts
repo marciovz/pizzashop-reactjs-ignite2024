4c201c53afe9d03119039e29c9d5f9f993776cf8
marciovz <marciovz@yahoo.com.br>
2024-04-28 14:06:42 -0300

    Mock de perfil e atualização

---
+++ src/api/mocks/get-profile-mock.ts
@@ -0,0 +1,17 @@
+import { http, HttpResponse } from "msw";
+import { GetProfileResponse } from '../get-profile';
+
+export const getProfileMock = http.get<never, never, GetProfileResponse>(
+  '/me', 
+  () => {
+    return HttpResponse.json({
+      id: 'custom-user-id',
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      phone: '11999988888',
+      role: 'manager',
+      createdAt: new Date(),
+      updateAt: null,
+    })
+  }
+)
