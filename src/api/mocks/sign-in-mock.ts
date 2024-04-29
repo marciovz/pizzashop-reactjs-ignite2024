8b5d37020f9a37859823a45f580062591d746962
marciovz <marciovz@yahoo.com.br>
2024-04-29 09:56:51 -0300

    Teste E2E: Sign In

--- src/api/mocks/sign-in-mock.ts
+++ src/api/mocks/sign-in-mock.ts
@@ -4,7 +4,7 @@ import { SignInBody } from '../sign-in';
 export const signInMock = http.post<never, SignInBody>('/authenticate', async ({ request }) => {
   const { email } = await request.json();
 
-  if(email === 'johndow@example.com') {
+  if(email === 'johndoe@example.com') {
     return new HttpResponse(null, {
       status: 200,
       headers: {
